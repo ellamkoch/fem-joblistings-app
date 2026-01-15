5a4281025c3668272a734bb10c49ee83468c0bdc
Ella <ellamkoch@gmail.com>
2026-01-14 21:45:37 -0600

    initial directories and files set up

---
+++ vitest.config.js
@@ -0,0 +1,41 @@
+import { defineConfig} from 'vitest/config';
+import react from "@vitejs/plugin-react";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        environment: 'jsdom',
+        globals: true,
+        clearMocks: true,
+        setupFiles: ["./test/setupTests.js"]
+        // ...
+        },
+    resolve: {
+         alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@hooks': path.resolve(__dirname, './src/hooks'),
+            '@lib': path.resolve(__dirname, './src/lib'),
+            '@styles': path.resolve(__dirname, './src/styles'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@tests': path.resolve(__dirname, './src/tests'),
+            '@utils': path.resolve(__dirname, './src/utils'),
+            "@test": path.resolve(__dirname, './test'),
+            "@providers": path.resolve(__dirname, './src/providers')
+        }
+      },
+            coverage: {
+                provider: "v8",
+                reporter: ["text", "json", "html"],// what reports/reviews the app
+                thresholds: {
+                    lines: 50,
+                    functions: 50,
+                    branches: 40,
+                    statements: 50
+                }
+        }
+});
